18c55b310ff24cc27faf40b783edea89e881116f
jbranchaud <jbranchaud@gmail.com>
2016-09-25 18:46:24 -0500

    Add some test setup and configurability

--- lib/print_board.ex
+++ lib/print_board.ex
@@ -1,10 +1,11 @@
 defmodule PrintBoard do
-  def print(board) do
-    live_char = Application.get_env(:golex, :internal_live_cell_character)
-    dead_char = Application.get_env(:golex, :internal_dead_cell_character)
-    printable_live_char = Application.get_env(:golex, :printable_live_cell_character)
-    printable_dead_char = Application.get_env(:golex, :printable_dead_cell_character)
-
+  def print(board), do: print(board, fetch_characters)
+  def print(board,
+   %{live_char: live_char,
+     dead_char: dead_char,
+     printable_live_char: printable_live_char,
+     printable_dead_char: printable_dead_char
+   }) do
     printable_board =
       Enum.map(board, fn(row) ->
         Enum.join(row)
@@ -13,5 +14,14 @@ defmodule PrintBoard do
       |> String.replace(dead_char, printable_dead_char)
       |> String.replace(live_char, printable_live_char)
     printable_board <> "\n"
+  end
+
+  defp fetch_characters do
+    %{
+      live_char: Application.get_env(:golex, :internal_live_cell_character),
+      dead_char: Application.get_env(:golex, :internal_dead_cell_character),
+      printable_live_char: Application.get_env(:golex, :printable_live_cell_character),
+      printable_dead_char: Application.get_env(:golex, :printable_dead_cell_character)
+    }
   end
 end
--- test/print_board_test.exs
+++ test/print_board_test.exs
@@ -1,8 +1,16 @@
 defmodule PrintBoardTest do
   use ExUnit.Case
 
-  test "step on an empty board" do
-    Application.put_env(:golex, :printable_dead_cell_character, "_")
+  setup do
+    {:ok, [
+      dead_char: "o",
+      live_char: "x",
+      printable_dead_char: "_",
+      printable_live_char: "•"
+    ]}
+  end
+
+  test "print a board with all dead cells", context do
     board = [
       [:o, :o, :o],
       [:o, :o, :o],
@@ -14,6 +22,21 @@ ___
 ___
 """
 
-    assert printed_board == PrintBoard.print(board)
+    assert printed_board == PrintBoard.print(board, context)
+  end
+
+  test "print a board with some live cells", context do
+    board = [
+      [:o, :x, :o],
+      [:x, :x, :x],
+      [:o, :x, :o]
+    ]
+    printed_board = """
+_•_
+•••
+_•_
+"""
+
+    assert printed_board == PrintBoard.print(board, context)
   end
 end
